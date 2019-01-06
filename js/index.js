4c701cfbc372708c62c75c89c4b8d2fc82ec364c
Avarka <vadavar7@gmail.com>
2019-01-06 21:09:01 +0100

    Listázások  + Feladatok kilistázása  + Csoportok kilistázása

--- js/index.js
+++ js/index.js
@@ -1,12 +1,15 @@
 function ListGroups() {
     var self = this;
     self.groupsURI = 'https://hazizz.duckdns.org:8081/me/groups';
+    self.grouptasksURI = 'https://hazizz.duckdns.org:8081/tasks/groups/';
+    self.permissionsURI = 'https://hazizz.duckdns.org:8081/me/permissions';
     if (Cookies.get('token')){
         self.token = Cookies.get('token');
     } else {
         window.location.href = "login.html";
     }
     self.groups = ko.observableArray();
+    self.alltasks = ko.observableArray();
 
     self.ajax = function (uri, method, data) {
         var request = {
@@ -42,14 +45,55 @@ function ListGroups() {
                 case "PASSWORD":
                     data[i].groupType = "Jelszó v."
                     break;
+                default:
+                    data[i].groupType = "Egyéb"
+                    break;
             }
             self.groups.push({
+                id: ko.observable(data[i].id),
                 name: ko.observable(data[i].name),
                 groupType: ko.observable(data[i].groupType),
                 userCount: ko.observable(data[i].userCount)
             });
         };
     });
+
+    self.tasks = function (group) {
+        console.log("Tasks got from: #" + group.id())
+        self.ajax(self.grouptasksURI + group.id(), 'GET').done(function (data) {
+            var rdata = [];
+            for (var i = 0; i < data.length; i++) {
+                switch (data[i].type.name) {
+                    case "homework":
+                        data[i].type.name = "Házi feladat";
+                        break;
+                    case "assigment":
+                        data[i].type.name = "Beadandó";
+                        break;
+                    case "test":
+                        data[i].type.name = "Teszt";
+                        break;
+                    case "oral test":
+                        data[i].type.name = "Felelet"
+                        break;
+                    default:
+                        data[i].type.name = "Egyéb";
+                        break;
+                }
+                data[i].dueDate = moment(data[i].dueDate, "YYYY-MM-DD").fromNow(true);
+                rdata.push({
+                    id: data[i].id,
+                    type: data[i].type.name,
+                    title: data[i].title,
+                    description: data[i].description,
+                    dueDate: data[i].dueDate,
+                    creator: data[i].creator.displayName,
+                    subject: data[i].subject
+                });
+            }
+            self.alltasks(rdata);
+        });
+    };
 };
 
 ko.applyBindings(new ListGroups(), $('#groups')[0]);
@@ -64,6 +108,7 @@ function AddGroups(){
     }
     self.groupName = ko.observable();
     self.groupType = ko.observable();
+    self.groupPassword = ko.observable();
 
     self.ajax = function (uri, method, data) {
         var request = {
@@ -71,7 +116,7 @@ function AddGroups(){
             type: method,
             contentType: "application/json",
             cache: false,
-            dataType: 'json',
+            dataType: 'text',
             data: JSON.stringify(data),
             beforeSend: function (xhr) {
                 xhr.setRequestHeader("Authorization", "Bearer " + self.token);
@@ -88,12 +133,18 @@ function AddGroups(){
     }
 
     self.create = function () {
-        console.log(self.groupName());
-        self.ajax(self.createURI, 'POST', {"groupName":self.groupName(), "type":self.groupType()}).done(function (data) {
-            console.log("New group: " + data);
-            $('#newGroupModal').modal('hide')
-        })
-    }
+        console.log("Group created! Group name: " + self.groupName() + ", group type: " + self.groupType());
+        var data;
+        if (self.groupType() == "PASSWORD"){
+            data = {"groupName":self.groupName(), "type":self.groupType(), "password":self.groupPassword()};
+        } else{
+            data = {"groupName":self.groupName(), "type":self.groupType()};
+        }
+        self.ajax(self.createURI, 'POST', data).done(function () {
+            $('#newGroupModal').modal('hide');
+            self.groupName("");
+        });
+    };
 };
 ko.applyBindings(new AddGroups(), $('#newGroup')[0]);
 
