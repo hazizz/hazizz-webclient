765d396512de7fbdc438577854efff38f16b54ac
Kornél (via Travis CI) <stefankornel02@gmail.com>
2019-10-26 18:10:56 +0000

    Deploy hazizz/hazizz-webclient to github.com/hazizz/hazizz-webclient.git:gh-pages

--- README.md
+++
@@ -1,1 +0,0 @@
-# Házizz Mindenhol PAGE
---
+++ precache-manifest.b7b06604e23076b8dc830db665ec6e80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7431e5f3bd2effef0e4694fd7abd9c82",
+    "url": "https://hazizz.github.io/hazizz-webclient/index.html"
+  },
+  {
+    "revision": "81dfa57f0136736845ea",
+    "url": "https://hazizz.github.io/hazizz-webclient/static/css/main.975147b4.chunk.css"
+  },
+  {
+    "revision": "d47c1be93df8a9859ab4",
+    "url": "https://hazizz.github.io/hazizz-webclient/static/js/2.fafcbea0.chunk.js"
+  },
+  {
+    "revision": "81dfa57f0136736845ea",
+    "url": "https://hazizz.github.io/hazizz-webclient/static/js/main.0d0923d2.chunk.js"
+  },
+  {
+    "revision": "ea62aa7b8e9c73a27949",
+    "url": "https://hazizz.github.io/hazizz-webclient/static/js/runtime-main.f6be953f.js"
+  },
+  {
+    "revision": "13001b8776df461b36ce08c48c16cbc9",
+    "url": "https://hazizz.github.io/hazizz-webclient/static/media/logo.13001b87.png"
+  }
+]);
