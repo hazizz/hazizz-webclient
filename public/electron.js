0d88e47d52da2b0c4804431e3c6489aa29a909f5
Kornél <stefankornel02@gmail.com>
2019-11-03 18:54:58 +0100

    Elecron basic configuration Social logins do not work, this builds.

---
+++ public/electron.js
@@ -0,0 +1,31 @@
+const electron = require('electron');
+const app = electron.app;
+app.commandLine.appendSwitch('ignore-certificate-errors', 'true');
+
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require('path');
+const url = require('url');
+const isDev = require('electron-is-dev');
+
+let mainWindow;
+
+function createWindow() {
+	mainWindow = new BrowserWindow({width: 900, height: 680});
+	mainWindow.loadURL(isDev ? 'https://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+	mainWindow.on('closed', () => mainWindow = null);
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
+
+app.on('activate', () => {
+	if (mainWindow === null) {
+		createWindow();
+	}
+});
