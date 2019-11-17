3444c35742a550d8c899f107db1a0a1d5a298f8b
Avarka <vadavar7@gmail.com>
2019-11-17 14:09:34 +0100

    Register

--- src/containers/Authenticate.tsx
+++ src/containers/Authenticate.tsx
@@ -1,4 +1,4 @@
-import React, {Component, ChangeEvent} from 'react';
+import React, {Component} from 'react';
 
 import GoogleLogin from "react-google-login";
 // @ts-ignore
@@ -13,37 +13,28 @@ import logo from '../assets/logo.png';
 
 import {AuthDetails, Error} from "../types/types";
 
-type Props = {
+import {RouteComponentProps} from "react-router-dom";
+
+interface Props extends RouteComponentProps {
     saveToken: (token: string, refresh: string, expires_in: number) => void,
-    location: any,
-    history: any
 }
 
-type State = {
-    consent: boolean,
-    withoutConsent: boolean,
-}
-
-class Authenticate extends Component<Props, State> {
-    constructor(props: Props) {
-        super(props);
-        this.state = {
-            consent: false,
-            withoutConsent: false,
-        }
-    }
-
-    handleCheckChange = (event: ChangeEvent<HTMLInputElement>) => {
-        this.setState({
-            consent: event.target.checked
-        })
-    };
-
+class Authenticate extends Component<Props> {
     render() {
         let {from} = this.props.location.state || {from: {pathname: "/home"}};
-        const consentClasses = ["consentLabel"];
-        if (this.state.withoutConsent)
-            consentClasses.push("text-red-500");
+
+        const handleTokenSave = (respData: AuthDetails) => {
+            this.props.saveToken(respData.token, respData.refresh, respData.expires_in);
+            this.props.history.replace(from);
+        };
+
+        const handleRegister = (respData: Error, type: string, token: string) => {
+            if (respData.errorCode === 23)
+                this.props.history.replace({
+                    pathname: this.props.location.pathname + "/consent",
+                    search: "?type=" + type + "&token=" + token
+                });
+        };
 
         const handleGoogleAuth = (resp: any) => {
             const tokenId:string = resp.tokenId;
@@ -57,50 +48,29 @@ class Authenticate extends Component<Props, State> {
                 }
             })
                 .then(resp => {
-                    const respData: AuthDetails = resp.data;
-                    this.props.saveToken(respData.token, respData.refresh, respData.expires_in);
-                    this.props.history.replace(from);
+                    handleTokenSave(resp.data);
                 })
                 .catch(resp => {
-                    const respData: Error = resp.response.data;
-                    if (respData.errorCode === 23){
-                        authInstance({
-                            url: "/account/googleregister",
-                            method: "post",
-                            data: {
-                                "openIdToken": tokenId,
-                                "consent": this.state.consent
-                            }
-                        })
-                            .then(resp => {
-                                const respData: AuthDetails = resp.data;
-                                this.props.saveToken(respData.token, respData.refresh, respData.expires_in);
-                                this.props.history.replace(from);
-                            })
-                            .catch(resp => {
-                                const respData: Error = resp.response.data;
-                                if (respData.errorCode === 35){
-                                    this.setState({withoutConsent: true});
-                                }
-                            });
-                    }
-                })
+                    handleRegister(resp.response.data, "google", tokenId);
+                });
         };
 
         const handleFacebookLogin = (resp: ReactFacebookLoginInfo) => {
+            const accessToken = resp.accessToken;
             authInstance({
                 url: "/auth",
                 method: "post",
                 params: {
                     "grant_type": "facebook_token",
-                    "facebook_token": resp.accessToken,
+                    "facebook_token": accessToken,
                     "client_id": "H_MINDENHOL"
                 }
             })
                 .then(resp => {
-                    const respData: AuthDetails = resp.data;
-                    this.props.saveToken(respData.token, respData.refresh, respData.expires_in);
-                    this.props.history.replace(from);
+                    handleTokenSave(resp.data);
+                })
+                .catch(resp => {
+                    handleRegister(resp.response.data, "facebook", accessToken);
                 });
         };
 
@@ -111,7 +81,7 @@ class Authenticate extends Component<Props, State> {
                     <h1 className="text-center text-3xl font-bold text-hazizz_blue-200 md:text-5xl lg:text-6xl">Jelentkezz
                         be!</h1>
                     <p className="text-xs text-center text-gray-600 tracking-tight md:text-sm md:tracking-normal lg:text-xl">
-                        (A bejelentkezést és a regisztációt ugyan azok a gombok végzik.)
+                        (Amennyiben nincs felhasználód a rendszer készít neked egyet.)
                     </p>
                     <p className="text-center text-hazizz_blue-200 text-xl font-semibold md:text-3xl lg:text-4xl">
                         Használd a közösségi bejelentkezések egyikét:
@@ -125,10 +95,9 @@ class Authenticate extends Component<Props, State> {
                                 size="small"
                                 render={(props: { onClick: () => React.MouseEvent }) => (
                                     <button
-                                        className={this.state.consent ? "btn bg-facebook cursor-pointer" : "btn bg-facebook cursor-default"}
+                                        className="btn btn-login bg-facebook"
                                         onClick={props.onClick}
-                                        disabled={!this.state.consent}
-                                        title={this.state.consent ? "" : "El kell fogadnod a feltételeket a folytatáshoz!"}>
+                                    >
                                         Bejelentkezés Facebook használatával
                                     </button>
                                 )}
@@ -141,31 +110,14 @@ class Authenticate extends Component<Props, State> {
                                 className="google_login_button"
                                 render={props => (
                                     <button
-                                        className={this.state.consent ? "btn bg-google cursor-pointer" : "btn bg-google cursor-default"}
+                                        className="btn btn-login bg-google"
                                         onClick={props.onClick}
-                                        disabled={!this.state.consent}
-                                        title={this.state.consent ? "" : "El kell fogadnod a feltételeket a folytatáshoz!"}>
+                                    >
                                         Bejelnetkezés Google használatával
                                     </button>
                                 )}
                             />
                         </div>
-                        <label className={consentClasses.join(" ")} htmlFor="consent">
-                            <input className="absolute opacity-0 cursor-pointer h-0 w-0"
-                                   onChange={this.handleCheckChange} checked={this.state.consent} type="checkbox"
-                                   name="consent" id="consent"/>
-                            <span className="consentCheckMark hover:bg-hazizz_blue-500 focus:shadow-outline"/>
-                            A regisztrációval és/vagy bejelekezéssel elfogadom a Házizz&nbsp;
-                            <a className="consentLink" rel="noopener noreferrer" target="_blank"
-                               href="https://raw.githubusercontent.com/hazizz/hazizz.github.io/master/privacy-hu.txt">adatvédelmi
-                                nyilatkozatát</a>,&nbsp;
-                            <a className="consentLink" rel="noopener noreferrer" target="_blank"
-                               href="https://raw.githubusercontent.com/hazizz/hazizz.github.io/master/tos-hu.txt">általános
-                                szerződési feltételeti</a>,
-                            és a&nbsp;
-                            <a className="consentLink" rel="noopener noreferrer" target="_blank"
-                               href="https://raw.githubusercontent.com/hazizz/hazizz.github.io/master/guideline-hu.txt">házirendjét</a>!
-                        </label>
                     </div>
                 </div>
             </div>
