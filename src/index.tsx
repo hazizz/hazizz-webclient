a93214bd7ad0511348b59184a725b77715a5a15b
Kornél <stefankornel02@gmail.com>
2020-02-17 13:39:56 +0100

    Auto redirect test

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import {HashRouter as Router, Switch, Route, Redirect} from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route, Redirect} from "react-router-dom";
 import PrivateRoute from "./components/PrivateRoute";
 import {Provider} from "react-redux";
 import store from "./store/store";
